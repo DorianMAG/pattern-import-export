9e421b97ba9b3d2cd1c8c127d08ea0bfa38b9ded
François Honoré <francois.honore@acsone.eu>
2020-06-11 08:45:03 +0200

    [12.0][IMP] Improve architecture of pattern_import_export by splitting this module in 2 parts: 1) Make a generic module with the purpose to only create the datastructure to prepare the export; 2) Make a specific xlsx module to export as Excel file (who is in charge to export with a specific format).
    (update also related tests)

--- pattern_import_export/__manifest__.py
+++ pattern_import_export/__manifest__.py
@@ -2,14 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Pattern Import Export",
-    "summary": "Pattern for import or export from or to excel files",
+    "summary": "Pattern for import or export",
     "version": "12.0.1.0.0",
     "category": "Extra Tools",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "http://www.akretion.com",
     "license": "AGPL-3",
     "depends": ["base", "queue_job", "document", "base_export_manager"],
-    "external_dependencies": {"python": ["xlsxwriter", "xlrd"]},
     "data": [
         "security/ir.model.access.csv",
         "wizard/export_with_pattern.xml",
